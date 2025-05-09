56b7b4338fba50f2c5ba386da9a33c529b10a4d8
Md. Rasadul Kabir <125783116+r-kabir-rafi@users.noreply.github.com>
2025-05-10 00:16:06 +0600

    Time: 219 ms (88.92%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
+
 SELECT 
     m1.machine_id,
     ROUND(AVG(m2.timestamp - m1.timestamp), 3) AS processing_time
 -- ROUND(SUM(a2.timestamp - a1.timestamp) / COUNT(a1.process_id), 3) AS processing_time
-
 FROM Activity AS m1
 JOIN Activity AS m2
     ON m1.machine_id = m2.machine_id
