7b0e0cec610787c2b72107b23ca7503f43eed266
Md. Rasadul Kabir <125783116+r-kabir-rafi@users.noreply.github.com>
2025-05-19 22:30:22 +0600

    Time: 168 ms (93.55%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -2,6 +2,7 @@
 
 SELECT patient_id, patient_name, conditions
 FROM Patients
-WHERE 
-       conditions LIKE 'DIAB1%' OR
-       conditions LIKE '% DIAB1%';
+WHERE
+    EXISTS( SELECT 1 FROM
+                unnest(string_to_array(conditions, ' ')) AS cond
+                WHERE cond LIKE 'DIAB1%');
