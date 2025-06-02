d806dfa80540cebe10166652d6dc83964bfa19d6
Md. Rasadul Kabir <125783116+r-kabir-rafi@users.noreply.github.com>
2025-06-02 11:31:45 +0600

    Time: 217 ms (33.52%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,11 +1,9 @@
 -- Write your PostgreSQL query statement below
 
-SELECT employee_id
-FROM Employees e 
-WHERE 
-        e.salary < 30000 
-    AND manager_id IS NOT NULL
-    AND manager_id NOT IN(
-        SELECT employee_id FROM Employees
-    )
-ORDER BY employee_id;
+SELECT e.employee_id
+FROM Employees e
+LEFT JOIN Employees m ON e.manager_id = m.employee_id
+WHERE e.salary < 30000
+  AND e.manager_id IS NOT NULL
+  AND m.employee_id IS NULL
+ORDER BY e.employee_id;
