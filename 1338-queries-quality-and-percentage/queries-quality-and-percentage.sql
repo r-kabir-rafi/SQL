2a6813fa73c62d7e788e2a4294e2b6a68b5c5951
Md. Rasadul Kabir <125783116+r-kabir-rafi@users.noreply.github.com>
2025-06-28 00:12:01 +0600

    Time: 230 ms (52.52%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,16 +1,7 @@
 -- Write your PostgreSQL query statement below
 
-SELECT 
-    query_name,
-    ROUND(AVG(rating * 1.0 / position), 2) AS quality,
-    ROUND(100.0 * poor / total, 2) AS poor_query_percentage
-FROM (
-    SELECT 
-        query_name,
-        rating,
-        position,
-        COUNT(*) OVER (PARTITION BY query_name) AS total,
-        SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) OVER (PARTITION BY query_name) AS poor
-    FROM Queries
-) AS sub
-GROUP BY query_name, poor, total;
+SELECT query_name, 
+        ROUND(AVG(rating * 1.0 / position),2) as quality,
+        ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)*100.0 / COUNT(*),2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
