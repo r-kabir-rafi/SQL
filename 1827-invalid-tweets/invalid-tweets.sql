e8e3800064630003048b49801d66548f33b8ab91
Md. Rasadul Kabir <125783116+r-kabir-rafi@users.noreply.github.com>
2025-07-04 00:55:21 +0600

    Time: 461 ms (10.32%) | Memory: 0B (100.00%) - LeetSync

--- 1827-invalid-tweets/invalid-tweets.sql
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -1,5 +1,8 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-SELECT tweet_id
-FROM Tweets
-WHERE LENGTH(content)>15;
+SELECT 
+    tweet_id
+FROM 
+    Tweets 
+WHERE   
+    LENGTH(content) > 15;
