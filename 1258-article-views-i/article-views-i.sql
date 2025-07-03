7fe1df195fe97ae0c55712c1d1d16813cd34a8eb
Md. Rasadul Kabir <125783116+r-kabir-rafi@users.noreply.github.com>
2025-07-04 00:43:33 +0600

    Time: 366 ms (44.88%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,8 +1,5 @@
 -- Write your PostgreSQL query statement below
 
-SELECT DISTINCT author_id as id
-FROM(
-    SELECT * 
-    FROM Views
-    WHERE author_id = viewer_id
-)
+SELECT DISTINCT author_id as id 
+FROM Views
+WHERE author_id = viewer_id;
