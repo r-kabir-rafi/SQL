bf8c92aecdcb65109b4b269c5c43c698cf2525c7
Md. Rasadul Kabir <125783116+r-kabir-rafi@users.noreply.github.com>
2025-07-04 20:01:54 +0600

    Time: 162 ms (84.53%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,13 +1,18 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
 SELECT 
-    m1.machine_id,
-    ROUND(AVG(m2.timestamp - m1.timestamp), 3) AS processing_time
--- ROUND(SUM(a2.timestamp - a1.timestamp) / COUNT(a1.process_id), 3) AS processing_time
-FROM Activity AS m1
-JOIN Activity AS m2
-    ON m1.machine_id = m2.machine_id
-    AND m1.process_id = m2.process_id
-    AND m1.activity_type = 'start'
-    AND m2.activity_type = 'end'
-GROUP BY m1.machine_id;
+    machine_id,
+    ROUND(AVG(end_t - start_t)::numeric, 3) AS processing_time
+FROM(
+    SELECT 
+        machine_id,
+        process_id, 
+        MAX(CASE WHEN activity_type = 'start' THEN timestamp END) as start_t,
+        MAX(CASE WHEN activity_type = 'end' THEN timestamp END) as end_t
+    FROM 
+        Activity a
+    GROUP BY
+        a.machine_id, a.process_id
+) as times
+GROUP BY 
+    machine_id;
