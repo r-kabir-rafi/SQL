616ad238e2504e124109697b6d3d1fd008816deb
Md. Rasadul Kabir <125783116+r-kabir-rafi@users.noreply.github.com>
2025-07-07 23:16:09 +0600

    Time: 296 ms (5.07%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,7 +1,16 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
-SELECT *
-FROM Cinema 
-WHERE MOD(id,2)=1
-    AND description != 'boring'
-ORDER BY rating DESC
+
+SELECT 
+    * 
+FROM 
+    Cinema
+WHERE 
+    id % 2 = 1
+    AND
+    description != 'boring'
+ORDER BY 
+    rating DESC;
+;
+
+
