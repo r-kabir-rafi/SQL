e21a5feafa6a3c1809b137404fb603ab720aeae8
Md. Rasadul Kabir <125783116+r-kabir-rafi@users.noreply.github.com>
2025-09-01 19:54:15 +0600

    Time: 344 ms (59.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,12 @@
+-- Write your PostgreSQL query statement below
+
+SELECT s.name
+FROM SalesPerson s
+where s.sales_id not in(
+SELECT sales_id
+FROM Orders o
+left join company c
+on o.com_id = c.com_id
+where c.name = 'RED'
+);
+
