60ba85b52900bea9b3d3fdda4fee3bee37a889be
Md. Rasadul Kabir <125783116+r-kabir-rafi@users.noreply.github.com>
2025-09-04 12:29:54 +0600

    Time: 233 ms (40.21%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,7 +1,11 @@
 -- Write your PostgreSQL query statement below
 
 
-SELECT email
-FROM Person 
-GROUP BY email
-HAVING COUNT(email) >1 ;
+SELECT 
+    email
+FROM
+    Person
+GROUP BY
+    email
+HAVING
+    count(*) > 1;
